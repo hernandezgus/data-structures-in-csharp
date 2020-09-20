3d695232e32c4fec5adaa85e11b357b1610fc82b
Gustavo Hernandez <gustavoh@onetree.com>
2020-09-20 13:23:45 -0300

    Adding new solution to repo

---
+++ data-structures-in-csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace data_structures_in_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
