9ee122090f16558e027a80b8ee1008b277d69d98
Gustavo Hernandez <gustavoh@onetree.com>
2020-09-20 13:26:08 -0300

    Adding MyLinkedList implementation

--- data-structures-in-csharp/Program.cs
+++ data-structures-in-csharp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using data_structures_in_csharp.Structures;
+using System;
 
 namespace data_structures_in_csharp
 {
@@ -6,7 +7,31 @@ namespace data_structures_in_csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MyLinkedListTest();
+        }
+
+        private static void MyLinkedListTest()
+        {
+            var list = new MyLinkedList<int>();
+            list.Add(1);
+            list.Add(5);
+            list.Add(12);
+            list.Add(15);
+
+            Console.WriteLine(list);
+
+            var (previous, node) = list.FindFirst(5);
+
+            Console.WriteLine(node.Value);
+            Console.WriteLine(node.Next.Value);
+
+            list.DeleteAfter(previous);
+
+            Console.WriteLine(list);
+
+            list.AddAfter(previous, 10);
+
+            Console.WriteLine(list);
         }
     }
 }
---
+++ data-structures-in-csharp/Structures/MyLinkedList.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+
+namespace data_structures_in_csharp.Structures
+{
+    public class MyLinkedList<T>
+    {
+        public MyNode<T> Root { get; private set; }
+
+        public MyNode<T> Add(T value)
+        {
+            var newNode = new MyNode<T>()
+            {
+                Value = value,
+                Next = null
+            };
+            if (Root == null)
+            {
+                Root = newNode;
+            } else
+            {
+                var node = Root;
+                while(node.Next != null)
+                {
+                    node = node.Next;
+                }
+                node.Next = newNode;
+            }
+            return newNode;
+        }
+
+        public MyNode<T> AddAfter(MyNode<T> node, T value)
+        {
+            var newNode = new MyNode<T>()
+            {
+                Value = value,
+                Next = null
+            };
+            if (node == null)
+            {
+                Root = newNode;
+            } else
+            {
+                newNode.Next = node.Next;
+                node.Next = newNode;
+            }
+            return newNode;
+        }
+
+        public (MyNode<T> previous, MyNode<T> found) FindFirst(T value)
+        {
+            MyNode<T> current = Root;
+            MyNode<T> previous = null;
+
+            if (current == null)
+            {
+                return (null, null);
+            }
+            if (current.Value.Equals(value))
+            {
+                return (null, Root);
+            }
+
+            do
+            {
+                previous = current;
+                current = current.Next;
+                if (current.Value.Equals(value))
+                {
+                    return (previous, current);
+                }
+            } while (null != current.Next);
+            return (null, null);
+        }
+        public bool DeleteAfter(MyNode<T> node)
+        {
+            if (node == null || node.Next == null)
+            {
+                return false;
+            }
+            var nextNode = node.Next;
+            node.Next = nextNode.Next;
+            return true;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder("[");
+            var node = Root;
+            while (node != null)
+            {
+                builder.Append($"{ node.Value }");
+                if (node.Next != null)
+                {
+                    builder.Append(",");
+                }
+                node = node.Next;
+            }
+            builder.Append("]");
+            return builder.ToString();
+        }
+    }
+
+    public class MyNode<T>
+    {
+        public MyNode<T> Next { get; set; }
+
+        public T Value { get; set; }
+    }
+}
